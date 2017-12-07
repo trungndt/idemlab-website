cb60d01e87008731658f2d27fc2c5e2effbaf7b5
Thanh Tuyen <tuyen.kyanon@gmail.com>
2017-12-07 16:41:54 +0700

    home page destop

---
+++ assets/coffee/sofitel.coffee
@@ -0,0 +1,8 @@
+$(".text-green").on "click", (event) ->
+  $("#event").show()
+  $("#hotel").hide()
+  event.preventDefault()
+$(".text-blue").on "click", (event) ->
+  $("#hotel").show()
+  $("#event").hide()
+  event.preventDefault()
--- assets/js/sofitel.js
+++ assets/js/sofitel.js
@@ -1,4 +1,14 @@
 (function() {
+  $(".text-green").on("click", function(event) {
+    $("#event").show();
+    $("#hotel").hide();
+    return event.preventDefault();
+  });
 
+  $(".text-blue").on("click", function(event) {
+    $("#hotel").show();
+    $("#event").hide();
+    return event.preventDefault();
+  });
 
 }).call(this);
