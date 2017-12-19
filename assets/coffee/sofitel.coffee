12e8123303716cac2e24f3206ea289a05846a935
Thanh Tuyen <tuyen.kyanon@gmail.com>
2017-12-19 17:55:49 +0700

    page 404 and form

--- assets/coffee/sofitel.coffee
+++ assets/coffee/sofitel.coffee
@@ -1,9 +1,9 @@
 $(".text-green").on "click", (event) ->
-  $("#event").show()
-  $("#hotel").hide()
+  $(".event").show()
+  $(".hotel").hide()
   event.preventDefault()
 $(".text-blue").on "click", (event) ->
-  $("#hotel").show()
-  $("#event").hide()
+  $(".hotel").show()
+  $(".event").hide()
   event.preventDefault()
 $('#subject').niceSelect()
--- assets/js/sofitel.js
+++ assets/js/sofitel.js
@@ -1,13 +1,13 @@
 (function() {
   $(".text-green").on("click", function(event) {
-    $("#event").show();
-    $("#hotel").hide();
+    $(".event").show();
+    $(".hotel").hide();
     return event.preventDefault();
   });
 
   $(".text-blue").on("click", function(event) {
-    $("#hotel").show();
-    $("#event").hide();
+    $(".hotel").show();
+    $(".event").hide();
     return event.preventDefault();
   });
 
